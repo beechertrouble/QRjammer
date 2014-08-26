ee001a77a6c2fc5221d1331a5c0f26eb1c4a368a
beecher <beechertrouble@gmail.com>
2014-08-26 17:48:45 -0400

    more amd, better layout, less public

--- QRjammer.js
+++ QRjammer.js
@@ -1,28 +1,121 @@
 /**
 *
 * QR Jammer
-* v 1.0
 * by BeecherTrouble : beechbot.com
 * special thanks to Google and Google Charts
 * requires jquery ...
 *
 */
-var QRjammer = (function() {
-	
-	var QR = {},
-		size,addCSS,appendTo,addButton,buttonSize,buttonClass,buttonAppendTo, JamJar;
-	
-	/* ===== funcs ==== */
-	QR.addJamCSS = function() {
+(function($, scope) {
 		
-		var css = '<style id="QRJ_style">'
-				+ '#QRJ_jamjar {position:fixed;top:0px;right:-20px;bottom:100%;opacity:0;left:0px;display:block;overflow-x:hidden;overflow-y:scroll;width:100%;background:rgba(0,0,0,0.5) no-repeat center center;transition:all 0.3s ease;}'
-				+ '#QRJ_jamjar.showQR{bottom:0%;opacity:1;}'
-				+ '#QRJ_jamjar_content {position:relative;display:block;text-align:center;padding:2em 2em 1em;color:#fff;background:#000;max-width:' + size + 'px;margin:4em auto 2em;}'
-				+ '#QRJ_jamjar_content img {position:relative;display:block;margin:0px auto;max-width:100%;}'
-				+ '#QRJ_shortURL_wrap {font-size:2em;padding:0.5em 0 0;line-height:1em;}'
-				+ '#QRJ_close {position:absolute;top:0.5em;right:0.5em;display:block;cursor:pointer;padding:1em 1em 0.75em;color:#fff;background:#000;background:rgba(0,0,0,0.25);line-height:1;}'
-				+ '.QRJ_button {background:#fff;color:#00;textdisplay:inline-block;position:relative;padding:0 0.25em;line-height:1;text-align:center;border:0px none !important;cursor:pointer;opacity:0.5;-moz-transition:opacity 0.4s ease;-webkit-transition:opacity 0.4s ease;-o-transition:opacity 0.4s ease;-ms-transition:opacity 0.4s ease;transition:opacity 0.4s ease;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;-o-box-sizing:border-box;-ms-box-sizing:border-box;box-sizing:border-box;}'
+	var QR = {
+			shortURL : undefined
+		},
+		size, 
+		addCSS, 
+		appendTo, 
+		addButton,
+		buttonSize,
+		buttonClass,
+		buttonAppendTo, 
+		JamJar = '<div id="QRJ_jamjar"><a id="QRJ_close">close</a><span id="QRJ_vcenter"></span><div id="QRJ_jamjar_content_wrap"><div id="QRJ_jamjar_content"></div></div></div>';
+	
+	/** 
+	* private
+	*/
+	function _addJamCSS(size) {
+		
+		var css = '<style id="QRJ_style" type="text/css">'
+				+ '#QRJ_jamjar,'
+				+ '#QRJ_jamjar_content,'
+				+ '#QRJ_jamjar_content img,'
+				+ '#QRJ_close'
+				+ '#QRJ_vcenter {'
+						+ 'position:relative;'
+						+ 'display:block;'
+					+ '}'
+				+ '#QRJ_jamjar {'
+						+ 'position:fixed;'
+						+ 'top:0px;'
+						+ 'right:-25px;'
+						+ 'bottom:100%;'
+						+ 'left:0px;'
+						+ 'padding:0 25px 0 0;'
+						+ 'overflow-x:hidden;'
+						+ 'overflow-y:scroll;'
+						+ 'width:107%;'
+						+ 'opacity:0;'
+						+ 'font-size:16px;'
+						+ 'background:rgba(0,0,0,0.5);'
+						+ 'box-sizing:content-box;'
+						+ 'transition:all 0.3s ease;'
+						+ 'z-index:999999999;'
+					+ '}'
+				+ '#QRJ_jamjar.showQR{'
+						+ 'bottom:0%;'
+						+ 'opacity:1;'
+					+ '}'
+				+ '#QRJ_vcenter {'
+						+ 'display:inline-block;'
+						+ 'height:100%;'
+						+ 'width:1px;'
+						+ 'margin-left:-5px;'
+						+ 'vertical-align:middle;'
+					+ '}'
+				+ '#QRJ_jamjar_content_wrap {'
+						+ 'display:inline-block;'
+						+ 'vertical-align:middle;'
+						+ 'width:100%;'
+					+ '}'
+				+ '#QRJ_jamjar_content {'
+						+ 'text-align:center;'
+						+ 'padding:2em 2em 1em;'
+						+ 'color:#fff;'
+						+ 'background:#000;'
+						+ 'max-width:' + size + 'px;'
+						+ 'margin:2em auto;'
+					+ '}'
+				+ '#QRJ_jamjar_content img {'
+						+ 'margin:0px auto;'
+						+ 'width:100%;'
+						+ 'height:auto;'
+						+ 'max-width:100%;'
+					+ '}'
+				+ '#QRJ_shortURL_wrap {'
+						+ 'font-size:1.5em;'
+						+ 'padding:0.5em 0 0;'
+						+ 'line-height:1;'
+					+ '}'
+				+ '#QRJ_close {'
+						+ 'position:fixed;'
+						+ 'top:0.5em;'
+						+ 'right:0.5em;'
+						+ 'cursor:pointer;'
+						+ 'padding:1em 1em 0.75em;'
+						+ 'color:#fff;'
+						+ 'background:rgba(0,0,0,0.25);'
+						+ 'line-height:1;'
+						+ 'transition:all 0.3s ease;'
+					+ '}'
+				+ '#QRJ_close:focus,'
+				+ '#QRJ_close:hover {'
+						+ 'background:rgba(0,0,0,0.5);'
+					+ '}'
+				+ '.QRJ_button {'
+						+ 'background:#fff;'
+						+ 'color:#000;'
+						+ 'display:inline-block;'
+						+ 'position:relative;'
+						+ 'padding:0 0.25em;'
+						+ 'line-height:1;'
+						+ 'text-align:center;'
+						+ 'border:0px none !important;'
+						+ 'cursor:pointer;'
+						+ 'opacity:0.5;'
+						+ 'transition:opacity 0.4s ease;'
+						+ 'box-sizing:border-box;'
+						+ 'z-index:999999999;'
+					+ '}'
 				+ '.QRJ_fixed-top-left {position:fixed;top:0.25em;left:0.25em;}'
 				+ '.QRJ_fixed-top-right {position:fixed;top:0.25em;right:0.25em;}'
 				+ '.QRJ_fixed-bottom-left {position:fixed;bottom:0.25em;left:0.25em;}'
@@ -32,8 +125,9 @@ var QRjammer = (function() {
 		if($("#QRJ_style").length <= 0)
 			$('head').prepend(css);
 		
-	} // addJamCSS
-	QR.addButton = function() {
+	}; // _addJamCSS()
+	
+	 function _addButton() {
 		
 		var button = '<a class="QRJ_button ' + buttonClass + '" title="click for QR and short URL" style="font-size:' + buttonSize + ';">&#9875;</a>';
 		
@@ -41,72 +135,122 @@ var QRjammer = (function() {
 		
 		if($(".QRJ_button").length <= 0)
 			el.append(button);
-			
-		$(".QRJ_button").on('click', function(e) { e.preventDefault(); QR.show(); });
-		
-	} // addButton
-	QR.addJamJar = function() {
-		
-		JamJar = '<div id="QRJ_jamjar"><a id="QRJ_close">close</a><div id="QRJ_jamjar_content"></div></div>';
-		
+					
+	}; // _addButton()
+	
+	function _addJamJar() {
+				
 		if($("#QRJ_jamjar").length <= 0)
 			$('body').append(JamJar);
-			
-		$("#QRJ_close").on('click', function(e) { e.preventDefault(); QR.hide(); });
+					
+	}; // _addJamJar()
+	
+	function _def(ting, def) {
 		
-	} // addJamJar
+		return ting !== undefined ? ting : def;
+		
+	}; // _def()
+	
+	function _bindings() {
+		
+		$(document).ready(function() {
+			
+			$(document.body)
+				.on('click', '#QRJ_jamjar_content', function(e) {
+					e.preventDefault();
+					e.stopPropagation();	
+				})
+				.on('click', "#QRJ_close, #QRJ_jamjar", function(e) { 
+					e.preventDefault(); 
+					QR.hide(); 
+				})
+				.on('click', ".QRJ_button", function(e) {
+					e.preventDefault();
+					QR.show(); 
+				});	
+		
+		});
+		
+	}; // _bindings();
+	
+	function _addQR() {
+		
+		// update QR?
+		QR.getQR();
+		
+		var shorty = QR.shortURL === undefined ? '' : QR.shortURL;
+						
+		$("#QRJ_jamjar_content").html('<img class="QRJ_img" src="' + QR.QRSRC + '" alt="QR for : ' + QR.loc + '" /><div id="QRJ_shortURL_wrap">' + shorty + '</div>');
+
+				
+	};  //
+	
+	/** 
+	* public
+	*/
 	QR.show = function() {
 		
-		QR.addQR();
+		if($("#QRJ_shortURL_wrap").length <= 0)		
+			_addQR();
 		
 		var el = !appendTo ? $("#QRJ_jamjar") : appendTo;
 		
 		el.addClass('showQR');
 
 		
-	} // show
+	}; // show
 	QR.hide = function() {
 		
 		var el = !appendTo ? $("#QRJ_jamjar") : appendTo;
 		
 		el.removeClass('showQR');
 		
-	} // hide
+	}; // hide
 	QR.init = function(args) {
 		
-		QR.defs = args || {};
+		args = _def(args, {});
 		
-		size = QR.defs.size || 300;
-		QR.loc = QR.defs.loc || window.location.href;
-		addCSS = QR.defs.addCSS || true;
-		appendTo = QR.defs.appendTo || false;
-		addButton = QR.defs.addButton || true;
-		buttonSize = QR.defs.buttonSize || '2em';
-		buttonClass = QR.defs.buttonClass || 'QRJ_fixed-bottom-right';
-		buttonAppendTo = QR.defs.buttonAppendTo || false;
-				
-		// thanks google!	
-		QR.QRSRC = 'https://chart.googleapis.com/chart?cht=qr&chs=' + size + 'x' + size + '&chl=' + escape(QR.loc);
-				
-		if(addCSS)
-			QR.addJamCSS();
-			
-		if(addButton)
-			QR.addButton();
-			
-		if(!appendTo) 
-			QR.addJamJar();
-			
-	} // init
-	QR.getQR = function() {
+		size = _def(args.size, 300);
+		addCSS = _def(args.addCSS, true);
+		appendTo = _def(args.appendTo, false);
+		addButton = _def(args.addButton, true);
+		buttonSize = _def(args.buttonSize, '2em');
+		buttonClass = _def(args.buttonClass, 'QRJ_fixed-bottom-right');
+		buttonAppendTo = _def(args.buttonAppendTo, false);
 		
-		QR.loc = QR.defs.loc || window.location.href;
-		QR.shortURL = QR.loc;
-		QR.getShortURL();
+		QR.loc = _def(args.loc,  window.location.href);
+		
+		$(document).ready(function() {		
+			
+			_bindings();
+					
+			if(addCSS)
+				_addJamCSS(size);
+				
+			if(addButton)
+				_addButton();
+				
+			if(!appendTo) 
+				_addJamJar();
+		});
+			
+	}; // init
+	QR.getQR = function(loc) {
+		
+		if(QR.loc === undefined) {
+			QR.loc = _def(loc, window.location.href);
+			QR.shortURL = QR.loc;
+		}
+		
+		if(!loc === undefined || QR.shortURL === undefined)
+			QR.getShortURL();
+			
 		QR.QRSRC = 'https://chart.googleapis.com/chart?cht=qr&chs=' + size + 'x' + size + '&chl=' + escape(QR.loc);	
 		
-	} // getQR
-	QR.getShortURL = function() {
+	}; // getQR
+	QR.getShortURL = function(loc) {
+		
+		QR.loc = _def(loc,  window.location.href);
 				
 		$.ajax('https://www.googleapis.com/urlshortener/v1/url', {
 			data : JSON.stringify( {"longUrl": QR.loc}),
@@ -118,21 +262,28 @@ var QRjammer = (function() {
 				QR.shortURL = d.id;
 				$("#QRJ_shortURL_wrap").text(QR.shortURL);
 				
+			},
+			error: function(e) {
+				
+				if(window.console !== undefined)
+					console.log(e);
+					
 			}
 		});
 		
-	} // getShortURL
-	QR.addQR = function() {
-		
-		// update QR?
-		QR.getQR();
-						
-		$("#QRJ_jamjar_content").html('<img class="QRJ_img" src="' + QR.QRSRC + '" alt="QR for : ' + QR.loc + '" /><div id="QRJ_shortURL_wrap"></div>');
-
-				
-	}  //
-
+	}; // getShortURL
+	
+	/**
+	* Apply the function to the supplied scope (window)
+	*/
+	scope.QRjammer = QR;
 	
 	return QR;
 			
-}()); // QRjammer	
+})(($ || jQuery), (typeof scope === 'undefined' ? window : scope)); // QRjammer	
+
+/**
+* Expose QRjammer as an AMD
+*/
+if (typeof define === "function") 
+	define("QRjammer", [], function () { return QRjammer; } );
